7861454e90529f6636b4530a75401765ac8018d5
Christophe Braud <christophe@agilitation.fr>
2017-06-02 12:03:44 +0200

    Extract campsi-service-docs from campsi

---
+++ README.md
@@ -0,0 +1,5 @@
+# campsi-service-docs
+[![Build Status](https://travis-ci.org/campsi/campsi-service-docs.svg?branch=master)](https://travis-ci.org/campsi/campsi-service-docs)
+
+
+Add documents service to campsi
---
+++ test/config/test.js
@@ -0,0 +1,29 @@
+module.exports = {
+    port: 3000,
+    host: 'http://localhost:3000',
+    campsi: {
+        title: 'Test Arezzo',
+        version: '1.0.0',
+        description: 'API de test avec les pizzas Arezzo !',
+        base_url: 'http://localhost:3000',
+        mongoURI: 'mongodb://localhost:27017/relationships',
+        license: {
+            name: 'MIT',
+            url: 'https://opensource.org/licenses/mit-license.php'
+        },
+        contact: {
+            name: 'Christophe Braud',
+            email: 'christophe@agilitation.fr',
+            url: 'http://agilitation.fr'
+        }
+    },
+    services: {
+        docs: {
+            title: 'Contenus',
+            description: 'Données sur les pizzas',
+            options: {
+                schema: require('../schema/test.json')
+            }
+        },
+    }
+};

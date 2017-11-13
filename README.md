ec77571b1ff25b5a961b62c62d9044c442beb176
Christophe Braud <christophe@agilitation.fr>
2017-11-13 11:21:11 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# campsi-service-trace
+[![Build Status][build-image]][build-url]
+[![Coverage Status][coverage-image]][coverage-url]
+[![Known Vulnerabilities][vulnerabilities-image]][vulnerabilities-url]
+
+Add Trace/Debug service to campsi
+
+[build-image]: https://travis-ci.org/campsi/campsi-service-trace.svg?branch=master
+[build-url]: https://travis-ci.org/campsi/campsi-service-trace
+
+[coverage-image]: https://coveralls.io/repos/github/campsi/campsi-service-trace/badge.svg?branch=master
+[coverage-url]: https://coveralls.io/github/campsi/campsi-service-trace?branch=master
+
+[vulnerabilities-image]: https://snyk.io/test/github/campsi/campsi-service-trace/badge.svg
+[vulnerabilities-url]: https://snyk.io/test/github/campsi/campsi-service-trace
---
+++ test/trace.js
@@ -0,0 +1,84 @@
+//During the test the env variable is set to private
+process.env.NODE_CONFIG_DIR = './test/config';
+process.env.NODE_ENV = 'test';
+
+//Require the dev-dependencies
+const debug = require('debug')('campsi:test');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const format = require('string-format');
+const CampsiServer = require('campsi');
+const config = require('config');
+const fs = require('fs');
+
+chai.should();
+let campsi;
+format.extend(String.prototype);
+chai.use(chaiHttp);
+
+const services = {
+    Trace: require('../lib'),
+};
+
+// Our parent block
+describe('Trace', () => {
+    beforeEach((done) => { //Before each test we empty the database
+        campsi = new CampsiServer(config.campsi);
+        campsi.mount('trace', new services.Trace(config.services.trace));
+        campsi.on('campsi/ready', () => {
+            done();
+        });
+        campsi.start()
+            .catch((err) => {
+                debug('Error: %s', err);
+            });
+    });
+    /*
+     * Test the /GET trace route
+     */
+    describe('/GET trace', () => {
+        it('it should return success', (done) => {
+            chai.request(campsi.app)
+                .get('/trace')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.should.be.json;
+                    res.body.should.be.a('object');
+                    done();
+                });
+        });
+    });
+    /*
+     * Test the /GET trace route
+     */
+    describe('/POST docs', () => {
+        it('it should return success', (done) => {
+            chai.request(campsi.app)
+                .post('/trace/foo/bar')
+                .set('content-type', 'application/json')
+                .send({testing: true, sender: 'mocha', deep: {description: 'more deeper object.'}})
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.should.be.json;
+                    res.body.should.be.a('object');
+                    done();
+                });
+        });
+    });
+    /*
+     * Test the /GET trace route
+     */
+    describe('/POST docs', () => {
+        it('it should return success', (done) => {
+            chai.request(campsi.app)
+                .post('/trace/file')
+                .attach('file', fs.readFileSync('./test/rsrc/test.txt'), 'test.txt')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.should.be.json;
+                    res.body.should.be.a('object');
+                    done();
+                });
+        });
+    });
+});

451352a6f4a42e352452b166f166ed94bfbfbe84
Christophe Braud <christophe@agilitation.fr>
2018-02-02 13:56:49 +0100

    Revert again. correction will be made in th branch (reverted from commit da12853616842af34ea0ae0a3b4b0bba8e1c758e) (reverted from commit fa69eb4cbb855eac5ca79d75964b518fc9e64529)

--- example/serve.js
+++ example/serve.js
@@ -1,8 +1,8 @@
 // This is a demo file presenting the authentification service
 const CampsiServer = require('campsi');
 const Auth = require('../lib');
-
-console.info(process.argv);
+const debug = require('debug')('example');
+debug(process.argv);
 const port = '3010'; // process.argv
 const host = 'localhost';
 const config = {
@@ -11,17 +11,26 @@ const config = {
     campsi: {
         title: 'Campsi Service Auth example server',
         base_url: 'http://' + host + ':' + port,
-        mongoURI: 'mongodb://localhost:27017/campsi-service-auth-example',
+        mongo: {
+            host: 'localhost',
+            port: 27017,
+            database: 'campsi-service-auth-example'
+        },
+
     },
     services: {
         auth: {
             title: 'Authentification',
             kind: 'auth',
             options: {
+                session: {
+                    secret: 'sqkerhgtkusyd'
+                },
                 providers: {
                     local: require('../lib/providers/local')({
                         baseUrl: 'http://' + host + ':' + port + '/auth',
-                        salt: 'CNDygyeFC6536964425994'
+                        salt: 'CNDygyeFC6536964425994',
+                        resetPasswordTokenExpiration: 10
                     })
                 }
             }
@@ -34,7 +43,11 @@ campsi.mount('auth', new Auth(config.services.auth));
 
 campsi.on('campsi/ready', () => {
     campsi.listen(config.port);
-    console.info('Campsi listening on port', config.port);
+    debug('Campsi listening on port', config.port);
+});
+
+campsi.on('auth/local/passwordResetTokenCreated', user => {
+    debug('passwordResetTokenCreated', user.identities.local.passwordResetToken);
 });
 
 campsi.start().catch((err) => {

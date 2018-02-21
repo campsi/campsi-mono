35660bf76be9cf1b48193b31b36dbf18e97e48ac
Christophe Braud <christophe@agilitation.fr>
2018-02-21 16:52:35 +0100

    update configuration (campsi update)

--- test/config/test.js
+++ test/config/test.js
@@ -7,9 +7,8 @@ module.exports = {
     host: 'http://localhost:3000',
     campsi: {
         title: 'Test - Campsi Service Docs',
-        version: '1.0.0',
         description: 'Test - Campsi Service Docs',
-        base_url: 'http://localhost:3000',
+        publicURL: 'http://localhost:3000',
         mongo: {
             host: 'localhost',
             port: 27017,

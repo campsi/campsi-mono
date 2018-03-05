6d0123a2dd2e72aab4994b87bb444be36ea14f8d
Christophe Braud <christophe@agilitation.fr>
2018-03-05 10:01:39 +0100

    Update Packages

--- test/config/test.js
+++ test/config/test.js
@@ -1,24 +1,13 @@
 module.exports = {
     port: 3000,
-    host: 'http://localhost:3000',
     campsi: {
         title: 'Test Arezzo',
-        version: '1.0.0',
         description: 'API de test avec les pizzas Arezzo !',
-        base_url: 'http://localhost:3000',
+        publicURL: 'http://localhost:3000',
         mongo: {
             'host': 'localhost',
             'port': 27017,
             'database': 'relationships'
-        },
-        license: {
-            name: 'MIT',
-            url: 'https://opensource.org/licenses/mit-license.php'
-        },
-        contact: {
-            name: 'Christophe Braud',
-            email: 'christophe@agilitation.fr',
-            url: 'http://agilitation.fr'
         }
     },
     services: {

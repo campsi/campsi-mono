9aca5cc4eba4f3926d543dddc1338eeadd3ade62
romainbessuges@gmail.com <x9pslAo>
2018-12-06 10:04:47 +0100

    Initial commit

---
+++ test/config/test.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  port: process.env.PORT || 3000,
+  campsi: {
+    title: 'Test - Campsi Service Stripe Billing',
+    description: 'Test - Campsi Service Stripe Billing',
+    publicURL: `http://localhost:${process.env.PORT || 3000}`,
+    mongo: {
+      host: 'localhost',
+      port: 27017,
+      database: 'test-campsi-service-billing'
+    }
+  },
+  services: {
+    billing: {
+      title: 'Contents',
+      description: 'Tested Service',
+      options: {
+        secret_key: 'sk_test_TLvEhxPpHyrPhJeyMnJyM9jj'
+      },
+    }
+  }
+};

878120188eafc85d66da1bfab8a562912c965ce4
Romain Batigne <roro@axeptio.eu>
2022-03-02 17:29:02 +0100

    refactor: attachResource for external usage
    to fake a service

--- services/versioned-docs/lib/param.js
+++ services/versioned-docs/lib/param.js
@@ -8,33 +8,37 @@ const createError = require('http-errors');
 
 module.exports.attachResource = function(options) {
   return (req, res, next) => {
-    if (req.params.resource) {
-      req.resource = options.resources[req.params.resource];
-
-      if (!req.resource)
-        throw new createError.NotFound(
-          `Resource ${req.params.resource} not found`
-        );
-
-      if (req.params.id) {
-        req.filter = { _id: createObjectId(req.params.id) };
-        if (!req.filter._id)
-          throw new createError.BadRequest("Can't recognize proper id");
-      }
-
-      req.groups = req.query?.groups
-        ? getValidGroupsFromString(req.query.groups)
-        : [];
-
-      // USER can access RESOURCE/FILTER with METHOD?
-
-      try {
-        const filter = can(req.user, req.resource, req.method);
-        req.filter = { ...req.filter, ...filter };
-        next();
-      } catch (err) {
-        throw new createError.Unauthorized(err.message);
-      }
-    }
+    this.attach(req, res, next, options);
   };
+};
+
+module.exports.attach = (req, res, next, options) => {
+  if (req.params.resource) {
+    req.resource = options.resources[req.params.resource];
+
+    if (!req.resource)
+      throw new createError.NotFound(
+        `Resource ${req.params.resource} not found`
+      );
+
+    if (req.params.id) {
+      req.filter = { _id: createObjectId(req.params.id) };
+      if (!req.filter._id)
+        throw new createError.BadRequest("Can't recognize proper id");
+    }
+
+    req.groups = req.query?.groups
+      ? getValidGroupsFromString(req.query.groups)
+      : [];
+
+    // USER can access RESOURCE/FILTER with METHOD?
+
+    try {
+      const filter = can(req.user, req.resource, req.method);
+      req.filter = { ...req.filter, ...filter };
+      next();
+    } catch (err) {
+      throw new createError.Unauthorized(err.message);
+    }
+  }
 };

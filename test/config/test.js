381e225bf0a2de4f6900b8b2e5a19099fb6e79ba
romain <romainbessuges@gmail.com>
2022-04-15 18:15:45 +0200

    Add bodyParser options for the different parsers (json, text, url)

--- test/config/test.js
+++ test/config/test.js
@@ -27,6 +27,11 @@ module.exports = {
     mongo: {
       uri: 'mongodb://localhost:27017/test-campsi',
       database: 'test-campsi'
+    },
+    bodyParser: {
+      json: {
+        limit: '10mb'
+      }
     }
   },
   campsiPrefix: {

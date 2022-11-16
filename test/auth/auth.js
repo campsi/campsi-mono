d468345cf5f4d43405e031c2346dfb35ad105ac4
James Lotery <jlotery@gmail.com>
2022-11-16 17:03:50 +0100

    fix: test file broken after merge conflict

--- test/auth/auth.js
+++ test/auth/auth.js
@@ -12,8 +12,6 @@ const createUser = require('../helpers/createUser');
 const debug = require('debug')('campsi:test');
 const setupBeforeEach = require('../helpers/setupBeforeEach');
 const { ObjectId } = require('mongodb');
-
-
 const { getUsersCollectionName } = require('../../services/auth/lib/modules/collectionNames');
 const expect = chai.expect;
 format.extend(String.prototype);
@@ -94,8 +92,6 @@ const expiredTokens2 = {
     grantedByProvider: 'local'
   }
 };
-
-
 const bob = {
   displayName: 'Bobby baton',
   email: 'b.baton@donuts.fr',
@@ -672,8 +668,6 @@ describe('Auth API', () => {
       }
     }).timeout(10000);
   });
-
-
   it('it should not let me remove expired tokens', async () => {
     const campsi = context.campsi;
     const res = await chai
@@ -808,89 +802,88 @@ describe('Auth API', () => {
       .toArray();
 
     expiredUserTokens.length.should.be.eq(6);
-   });
+  });
+});
+
+describe('extract user personal data', () => {
+  it('should extract personal data', async () => {
+    const campsi = context.campsi;
+    const admin = {
+      email: 'admin@campsi.io',
+      username: 'admin@campsi.io',
+      displayName: 'admin',
+      password: 'password'
+    };
+
+    // create two users, one admin one lamba
+    const adminToken = await createUser(chai, campsi, admin, true);
+    await campsi.db.collection('__users__').findOneAndUpdate({ email: admin.email }, { $set: { isAdmin: true } });
+
+    await createUser(chai, campsi, glenda);
+
+    // add facebook and google identites to glenda
+    await campsi.db
+      .collection('__users__')
+      .findOneAndUpdate(
+        { email: glenda.email },
+        { $set: { 'identities.facebook': identities.facebook, 'identities.google': identities.google } }
+      );
+
+    let res = await chai
+      .request(campsi.app)
+      .get('/auth/users')
+      .set('Authorization', 'Bearer ' + adminToken);
+
+    res.should.have.status(200);
+
+    // get glenda's id
+    const glendaId = res.body.filter(u => u.email === glenda.email)[0]._id;
+
+    // get the personal data based on user id
+    res = await chai
+      .request(campsi.app)
+      .get(`/auth/users/${glendaId}/extract_personal_data`)
+      .set('Authorization', 'Bearer ' + adminToken);
+
+    res.body.identities.local.id.should.equal(glenda.username);
+    res.body.identities.local.username.should.equal(glenda.username);
+    res.body.identities.facebook.name.should.equal(identities.facebook.name);
+    res.body.identities.facebook.email.should.equal(identities.facebook.email);
+    res.body.identities.google.name.should.equal(identities.google.name);
+    res.body.identities.google.given_name.should.equal(identities.google.given_name);
+    res.body.identities.google.picture.should.equal(identities.google.picture);
+    res.body.identities.google.email.should.equal(identities.google.email);
   });
 
-  describe('extract user personal data', () => {
-    it('should extract personal data', async () => {
-      const campsi = context.campsi;
-      const admin = {
-        email: 'admin@campsi.io',
-        username: 'admin@campsi.io',
-        displayName: 'admin',
-        password: 'password'
-      };
+  it('should return a not authorized error', async () => {
+    const campsi = context.campsi;
+    const admin = {
+      email: 'admin@campsi.io',
+      username: 'admin@campsi.io',
+      displayName: 'admin',
+      password: 'password'
+    };
 
-      // create two users, one admin one lamba
-      const adminToken = await createUser(chai, campsi, admin, true);
-      await campsi.db.collection('__users__').findOneAndUpdate({ email: admin.email }, { $set: { isAdmin: true } });
+    // create two users one admin one lambda
+    const adminToken = await createUser(chai, campsi, admin, true);
+    await campsi.db.collection('__users__').findOneAndUpdate({ email: admin.email }, { $set: { isAdmin: true } });
 
-      await createUser(chai, campsi, glenda);
+    const glendaToken = await createUser(chai, campsi, glenda);
 
-      // add facebook and google identites to glenda
-      await campsi.db
-        .collection('__users__')
-        .findOneAndUpdate(
-          { email: glenda.email },
-          { $set: { 'identities.facebook': identities.facebook, 'identities.google': identities.google } }
-        );
+    let res = await chai
+      .request(campsi.app)
+      .get('/auth/users')
+      .set('Authorization', 'Bearer ' + adminToken);
 
-      let res = await chai
-        .request(campsi.app)
-        .get('/auth/users')
-        .set('Authorization', 'Bearer ' + adminToken);
+    res.should.have.status(200);
 
-      res.should.have.status(200);
+    // get glenda's id
+    const glendaId = res.body.filter(u => u.email === glenda.email)[0]._id;
 
-      // get glenda's id
-      const glendaId = res.body.filter(u => u.email === glenda.email)[0]._id;
-
-      // get the personal data based on user id
-      res = await chai
-        .request(campsi.app)
-        .get(`/auth/users/${glendaId}/extract_personal_data`)
-        .set('Authorization', 'Bearer ' + adminToken);
-
-      res.body.identities.local.id.should.equal(glenda.username);
-      res.body.identities.local.username.should.equal(glenda.username);
-      res.body.identities.facebook.name.should.equal(identities.facebook.name);
-      res.body.identities.facebook.email.should.equal(identities.facebook.email);
-      res.body.identities.google.name.should.equal(identities.google.name);
-      res.body.identities.google.given_name.should.equal(identities.google.given_name);
-      res.body.identities.google.picture.should.equal(identities.google.picture);
-      res.body.identities.google.email.should.equal(identities.google.email);
-    });
-
-    it('should return a not authorized error', async () => {
-      const campsi = context.campsi;
-      const admin = {
-        email: 'admin@campsi.io',
-        username: 'admin@campsi.io',
-        displayName: 'admin',
-        password: 'password'
-      };
-
-      // create two users one admin one lambda
-      const adminToken = await createUser(chai, campsi, admin, true);
-      await campsi.db.collection('__users__').findOneAndUpdate({ email: admin.email }, { $set: { isAdmin: true } });
-
-      const glendaToken = await createUser(chai, campsi, glenda);
-
-      let res = await chai
-        .request(campsi.app)
-        .get('/auth/users')
-        .set('Authorization', 'Bearer ' + adminToken);
-
-      res.should.have.status(200);
-
-      // get glenda's id
-      const glendaId = res.body.filter(u => u.email === glenda.email)[0]._id;
-
-      // should return a 401 error
-      res = await chai
-        .request(campsi.app)
-        .get(`/auth/users/${glendaId}/extract_personal_data`)
-        .set('Authorization', 'Bearer ' + glendaToken);
-    });
+    // should return a 401 error
+    res = await chai
+      .request(campsi.app)
+      .get(`/auth/users/${glendaId}/extract_personal_data`)
+      .set('Authorization', 'Bearer ' + glendaToken);
   });
-  });
+});

f501538824053ec796622830032337c0162bd9bf
James Lotery <jlotery@gmail.com>
2022-11-29 13:00:13 +0100

    chore: add some helper classes

---
+++ services/audit/utils.js
@@ -0,0 +1,30 @@
+// base validation schema, can be overriden in the service options
+const defaultSchema = {
+  type: 'object',
+  additionalProperties: false,
+  properties: {
+    action: {
+      type: 'string',
+      enum: ['CREATE', 'DELETE', 'MODIFY', 'READ']
+    },
+    data: {
+      type: 'object'
+    },
+    user: {
+      bsonType: 'objectId'
+    },
+    date: {
+      bsonType: 'date'
+    }
+  },
+  required: ['name', 'data', 'user', 'date']
+};
+
+module.exports.getCollectionName = function getCollectionName(options) {
+  // if no collection name specified in the options file default to 'audit'
+  return options?.collectionName ?? 'audit';
+};
+
+module.exports.validationSchema = function validationSchema(options) {
+  return options?.schema ?? defaultSchema;
+};

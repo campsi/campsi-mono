80f03a28c334ad7e8bb90e1735d3795db3b54e27
James Lotery <jlotery@gmail.com>
2022-11-29 12:59:01 +0100

    chore: basic unit tests

--- test/audit/service_creation.js
+++ test/audit/service_creation.js
@@ -6,7 +6,7 @@ process.env.NODE_ENV = 'test';
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const config = require('config');
-const debug = require('debug')('campsi:test');
+const fakeObjectId = require('fake-object-id');
 const setupBeforeEach = require('../helpers/setupBeforeEach');
 chai.use(chaiHttp);
 chai.should();
@@ -17,8 +17,8 @@ const services = {
 
 describe('Audit Service', () => {
   const context = {};
-  beforeEach(setupBeforeEach(config, services, context));
-  afterEach(done => {
+  before(setupBeforeEach(config, services, context));
+  after(done => {
     context.server.close(done);
   });
 
@@ -26,14 +26,69 @@ describe('Audit Service', () => {
    * Test the /GET route
    */
   describe('/GET', () => {
-    it('should return the service description', async () => {
-      const res = chai.request(context.campsi.app).get('/');
+    it('should create a journal entry via a REST call', async () => {
+      const entry = {
+        action: 'CREATE',
+        data: {
+          id: fakeObjectId()
+        },
+        user: fakeObjectId(),
+        date: new Date().toISOString()
+      };
+
+      const res = await chai.request(context.campsi.app).post('/audit/log').set('content-type', 'application/json').send(entry);
+
+      res.should.have.status(200);
+      res.should.be.json;
+    });
+
+    it('should retrieve a journal entry via a REST CALL', async () => {
+      const startDate = new Date();
+      const endDate = new Date();
+
+      startDate.setSeconds(startDate.getSeconds() - 10);
+      endDate.setSeconds(endDate.getSeconds() + 10);
+
+      const res = await chai
+        .request(context.campsi.app)
+        .get(`/audit/log/?startDate=${startDate.toUTCString()}&endDate=${endDate.toUTCString()}`)
+        .set('content-type', 'application/json');
+
       res.should.have.status(200);
       res.should.be.json;
       res.body.should.be.a('object');
-      res.body.services.notifications.should.be.a('object');
-      res.body.services.notifications.title.should.be.eq('Audit Service');
-      res.body.services.notifications.class.should.be.eq('AuditService');
+    });
+
+    it('should retrieve no journal entries via a REST CALL', async () => {
+      const startDate = new Date('2022-01-01');
+      const endDate = new Date('2022-02-01');
+
+      startDate.setSeconds(startDate.getSeconds() - 10);
+      endDate.setSeconds(endDate.getSeconds() + 10);
+
+      const res = await chai
+        .request(context.campsi.app)
+        .get(`/audit/log/?startDate=${startDate.toUTCString()}&endDate=${endDate.toUTCString()}`)
+        .set('content-type', 'application/json');
+
+      res.should.have.status(200);
+      res.should.be.json;
+      res.body.should.be.empty;
+    });
+
+    it('should create a journal entry via the audit lib', async () => {
+      const entry = {
+        action: 'CREATE',
+        data: {
+          name: 'james'
+        },
+        user: fakeObjectId(),
+        date: new Date().toISOString()
+      };
+
+      console.log(entry);
+      const auditService = context.campsi.services.get('audit');
+      auditService.createLog(entry);
     });
   });
 });

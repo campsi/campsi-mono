906d4de620c5d23e33c0fcda809cdad8ab2ce077
Romain Batigne <roro@axeptio.eu>
2023-08-28 16:06:39 +0200

    feat: S3AssetStorage refactored

--- services/assets/lib/storages/s3.js
+++ services/assets/lib/storages/s3.js
@@ -1,5 +1,6 @@
 /* eslint-disable node/no-unsupported-features/es-syntax */
-const aws = require('aws-sdk');
+const { S3Client, DeleteObjectCommand, GetObjectCommand } = require('@aws-sdk/client-s3');
+const { Upload } = require('@aws-sdk/lib-storage');
 const AssetStorage = require('../storage');
 const { PassThrough } = require('stream');
 const { randomUUID: uuid } = require('crypto');
@@ -12,6 +13,15 @@ const debug = require('debug')('campsi:assets:s3');
  */
 
 class S3AssetStorage extends AssetStorage {
+  constructor(options) {
+    super(options);
+    this.s3 = new S3Client({
+      region: this.options.region || this.options.credentials?.region, // handle old config
+      credentials: this.options.credentials,
+      maxAttempts: this.options.maxAttempts || 3
+    });
+  }
+
   get dataPath() {
     return this.options.dataPath;
   }
@@ -32,7 +42,7 @@ class S3AssetStorage extends AssetStorage {
 
   createPassThrough(file) {
     const getPublicAssetURL = this.options.getPublicAssetURL;
-    const s3 = new aws.S3({ ...this.options.credentials });
+
     const bucket = this.options.bucket;
     // will be used later in a scoped context, do not remove
     const getKey = this.getKey;
@@ -46,54 +56,56 @@ class S3AssetStorage extends AssetStorage {
       .on('error', function handleError(err) {
         debug(err);
       })
-      .on('finish', function streamBuffered() {
+      .on('finish', async function streamBuffered() {
         const self = this;
-        s3.upload({
-          Bucket: bucket,
-          Key: getKey(file, false),
-          ContentType: file.detectedMimeType || 'application/octet-stream',
-          ContentLength: file.size,
-          Body: buffer
-        })
-          .on('httpUploadProgress', function (ev) {
-            if (ev.total) file.uploadedSize = ev.total;
-          })
-          .send((err, data) => {
-            if (err) {
-              return self.emit('uploadError', err);
+        try {
+          const upload = new Upload({
+            client: this.s3,
+            params: {
+              Bucket: bucket,
+              Key: getKey(file, false),
+              ContentType: file.detectedMimeType || 'application/octet-stream',
+              ContentLength: file.size,
+              Body: buffer
             }
-            file.s3 = data;
-            file.url =
-              typeof getPublicAssetURL === 'function' ? getPublicAssetURL({ ...data, encodedKey: getKey(file) }) : data.Location;
-            self.emit('uploadSuccess', file);
+          }).on('httpUploadProgress', function (ev) {
+            if (ev.total) file.uploadedSize = ev.total;
           });
+
+          const data = await upload.done();
+          file.s3 = data;
+          file.url =
+            typeof getPublicAssetURL === 'function'
+              ? getPublicAssetURL({ key: data?.Key, ...data, encodedKey: getKey(file) }) // key: handle old return value
+              : data.Location;
+          self.emit('uploadSuccess', file);
+        } catch (err) {
+          return self.emit('uploadError', err);
+        }
       });
   }
 
-  deleteAsset(file) {
-    return new Promise((resolve, reject) => {
-      this.s3.deleteObject(
-        {
-          Bucket: this.options.bucket,
-          Key: file.key
-        },
-        err => (err ? reject(err) : resolve())
-      );
-    });
+  async deleteAsset(file) {
+    await this.s3.send(
+      new DeleteObjectCommand({
+        Bucket: this.options.bucket,
+        Key: file.key
+      })
+    );
   }
 
   getAssetURL(asset) {
     return asset.url;
   }
 
-  streamAsset(asset) {
-    const s3 = new aws.S3({ ...this.options.credentials });
-    return s3
-      .getObject({
+  async streamAsset(asset) {
+    const data = await this.s3.send(
+      new GetObjectCommand({
         Bucket: asset.s3.Bucket,
         Key: asset.s3.Key
       })
-      .createReadStream();
+    );
+    return data.Body;
   }
 }
 

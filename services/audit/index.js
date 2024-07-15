f263956cc47adf5002f5ce31070c069b252398f6
Romain Batigne <roro@axeptio.eu>
2024-07-15 13:46:07 +0200

    feat: audit service initialize() refactored

--- services/audit/index.js
+++ services/audit/index.js
@@ -7,49 +7,26 @@ const JournalService = require('./services/journal');
 const Ajv = require('ajv');
 const addFormats = require('ajv-formats');
 const ajvErrors = require('ajv-errors');
+const csdAssign = require('../../lib/keywords/csdAssign');
+const csdVisibility = require('../../lib/keywords/csdVisibility');
+const $RefParser = require('json-schema-ref-parser');
 
 module.exports = class AuditService extends CampsiService {
-  initialize() {
+  async initialize() {
     debug('initialize audit service');
     this.createLog = this.createLog.bind(this);
     this.setupRoutes = this.setupRoutes.bind(this);
-    this.setupSchemaValidation = this.setupSchemaValidation.bind(this);
 
     const service = this;
 
     this._journalService = JournalService;
 
-    this.db.collection(utils.getCollectionName(this.options)).createIndex({ date: -1 });
+    this.setupRoutes(service);
 
-    if (
-      Object.prototype.toString.call(this.options.ttlIndex) === '[object Object]' &&
-      typeof this.options.ttlIndex.field === 'string' &&
-      Number.isInteger(this.options.ttlIndex.expireAfterSeconds) &&
-      this.options.ttlIndex.expireAfterSeconds >= 0
-    ) {
-      this.db
-        .collection(utils.getCollectionName(this.options))
-        .createIndex({ [this.options.ttlIndex.field]: 1 }, { expireAfterSeconds: this.options.ttlIndex.expireAfterSeconds });
-    }
+    await this.createIndexes();
+    await this.addSchemaValidationToResources();
 
-    return Promise.all(
-      Object.entries(service.options.resources).map(async ([key, resource]) => {
-        const schema = await utils.validationSchema(service, resource);
-        this.setupSchemaValidation(resource, schema);
-      })
-    ).then(() => this.setupRoutes(service));
-  }
-
-  setupSchemaValidation(resource, schema) {
-    const ajvReader = new Ajv({ allErrors: true, useAssign: true, strictTuples: false, strict: false });
-    ajvErrors(ajvReader);
-    addFormats(ajvReader, ['date-time']);
-
-    try {
-      resource.validate = ajvReader.compile(schema);
-    } catch (ex) {
-      console.log(ex);
-    }
+    return super.initialize();
   }
 
   setupRoutes(service) {
@@ -62,6 +39,31 @@ module.exports = class AuditService extends CampsiService {
 
     this.router.get('/log', handlers.getLog);
     this.router.post('/log', handlers.createLogEntry);
+  }
+
+  async addSchemaValidationToResources() {
+    const ajvReader = new Ajv({ allErrors: true, useAssign: true, strictTuples: false, strict: false });
+    ajvErrors(ajvReader);
+    addFormats(ajvReader, ['date-time']);
+
+    for (const resource of Object.values(this.options.resources)) {
+      const schema = await utils.validationSchema(this, resource);
+      resource.validate = ajvReader.compile(schema);
+    }
+  }
+
+  async createIndexes() {
+    await this.db.collection(utils.getCollectionName(this.options)).createIndex({ date: -1 });
+    if (
+      Object.prototype.toString.call(this.options.ttlIndex) === '[object Object]' &&
+      typeof this.options.ttlIndex.field === 'string' &&
+      Number.isInteger(this.options.ttlIndex.expireAfterSeconds) &&
+      this.options.ttlIndex.expireAfterSeconds >= 0
+    ) {
+      await this.db
+        .collection(utils.getCollectionName(this.options))
+        .createIndex({ [this.options.ttlIndex.field]: 1 }, { expireAfterSeconds: this.options.ttlIndex.expireAfterSeconds });
+    }
   }
 
   async createLog(body) {
